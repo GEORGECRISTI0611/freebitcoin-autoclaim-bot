a14e6e09882e46bd49b18c698e238af594912a79
salandre <benoit.salandre@gmail.com>
2022-03-06 23:31:42 +0100

    first commit to github

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+using System.IO;
+
+namespace FreeBitcoinBot2
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //For Windows 7 and above, best to include relevant app.manifest entries as well
+            Cef.EnableHighDPISupport();
+
+            var settings = new CefSettings()
+            {
+                //By default CefSharp will use an in-memory cache, you need to specify a Cache Folder to persist data
+                CachePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "CefSharp\\Cache")
+            };
+
+            //Example of setting a command line argument
+            //Enables WebRTC
+            settings.CefCommandLineArgs.Add("enable-media-stream", "1");
+            settings.PersistSessionCookies = true;
+
+            //Perform dependency check to make sure all relevant resources are in our output directory.
+            Cef.Initialize(settings, performDependencyCheck: true, browserProcessHandler: null);
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Form WB = new cromiumForm();
+            WB.Show();
+            Application.Run();
+        }
+    }
+}
---
+++ cromiumForm.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using CefSharp.WinForms;
+using CefSharp;
+
+namespace FreeBitcoinBot2
+{
+    public partial class cromiumForm : Form
+    {
+        private readonly ChromiumWebBrowser browser;
+
+        private Timer hourTimer = new Timer();
+
+        public cromiumForm()
+        {
+            InitializeComponent();
+
+            hourTimer.Tick += new EventHandler(hourTimer_Tick);
+            hourTimer.Interval = 60000 + getSec();
+
+            browser = new ChromiumWebBrowser("https://freebitco.in/?op=home");
+
+            //toolStripContainer.ContentPanel.Controls.Add(browser);
+            this.Controls.Add(browser);
+            browser.Dock = DockStyle.Fill;
+
+            browser.IsBrowserInitializedChanged += OnIsBrowserInitializedChanged;
+            browser.LoadingStateChanged += OnLoadingStateChanged;
+            browser.ConsoleMessage += OnBrowserConsoleMessage;
+            browser.StatusMessage += OnBrowserStatusMessage;
+            browser.TitleChanged += OnBrowserTitleChanged;
+            browser.AddressChanged += OnBrowserAddressChanged;
+
+            hourTimer.Start();
+        }
+
+        private int getSec()
+        {
+            Random rnd = new Random();
+            return rnd.Next(0, 1) * 1000;
+        }
+
+        private void hourTimer_Tick(object sender, EventArgs e)
+        {
+            int heures = DateTime.Now.Hour;
+
+            hourTimer.Interval = 60000 + getSec();
+            browser.Load("https://freebitco.in/?op=home");
+        }
+
+        private void OnIsBrowserInitializedChanged(object sender, EventArgs e)
+        {
+            var b = ((ChromiumWebBrowser)sender);
+
+            //this.InvokeOnUiThreadIfRequired(() => b.Focus());
+        }
+
+        private void OnBrowserConsoleMessage(object sender, ConsoleMessageEventArgs args)
+        {
+            DisplayOutput(string.Format("Line: {0}, Source: {1}, Message: {2}", args.Line, args.Source, args.Message));
+        }
+
+        private void OnBrowserStatusMessage(object sender, StatusMessageEventArgs args)
+        {
+
+        }
+
+        private async void OnLoadingStateChanged(object sender, LoadingStateChangedEventArgs args)
+        {
+            SetCanGoBack(args.CanGoBack);
+            SetCanGoForward(args.CanGoForward);
+
+            if (!args.IsLoading)
+            {
+                try
+                {
+                    browser.ExecuteScriptAsync("SwitchPageTabs('free_play')");
+                    sleep(2000);
+
+                    string script = "document.getElementById('free_play_form_button').getAttribute('style')";
+                    JavascriptResponse response = await browser.EvaluateScriptAsync(script);
+
+                    if (response.Result == null)
+                    {
+                        browser.ExecuteScriptAsync("document.getElementById('free_play_form_button').click();");
+                        sleep(1000);
+                    }
+
+                }
+                catch (Exception e) {
+                    Console.Write("Erreur : " + e.Message);
+                }
+            }
+        }
+
+        private void OnBrowserTitleChanged(object sender, TitleChangedEventArgs args)
+        {
+            //this.InvokeOnUiThreadIfRequired(() => Text = args.Title);
+        }
+
+        private void OnBrowserAddressChanged(object sender, AddressChangedEventArgs args)
+        {
+
+        }
+
+        private void SetCanGoBack(bool canGoBack)
+        {
+
+        }
+
+        private void SetCanGoForward(bool canGoForward)
+        {
+
+        }
+
+        public void DisplayOutput(string output)
+        {
+
+        }
+
+        private void GoButtonClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BackButtonClick(object sender, EventArgs e)
+        {
+            browser.Back();
+        }
+
+        private void ForwardButtonClick(object sender, EventArgs e)
+        {
+            browser.Forward();
+        }
+
+        private void UrlTextBoxKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode != Keys.Enter)
+            {
+                return;
+            }
+        }
+
+        private void LoadUrl(string url)
+        {
+            if (Uri.IsWellFormedUriString(url, UriKind.RelativeOrAbsolute))
+            {
+                browser.Load(url);
+            }
+        }
+
+        private void cromiumForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            browser.Dispose();
+            Cef.Shutdown();
+            Application.Exit();
+        }
+
+        private void sleep(int numMilliSeconds)
+        {
+            System.Threading.Thread.Sleep(numMilliSeconds);
+        }
+    }
+}
